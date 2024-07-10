2e58b7c87e1630684a4675b558d9fc84df4f0c38
İbrahim Akar <110459159+akaribrahim4317@users.noreply.github.com>
2024-07-10 18:20:03 +0300

    commit

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -1,14 +1,33 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
-import { wp } from '../utils';
+import { hp, wp } from '../utils';
 import { navigate } from '../utils/navigation.util';
+import { Colors, Users } from '../constants';
+
 
 const HomeScreen: React.FC = () => {
+  const randomNumber = Math.floor(Math.random() * 10)
+
+
+  console.log(Users)
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => navigate('AddScreen')} style={styles.imageContainer} >
-        <Image source={require('../../assets/images/add.png')} style={styles.image} />
-      </TouchableOpacity>
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginHorizontal: wp(5) }} >
+        <Text style={styles.title} >Çalışan Listesi</Text>
+        <TouchableOpacity onPress={() => navigate('AddScreen')} style={styles.imageContainer} >
+
+          <Image source={require('../../assets/images/add.png')} style={styles.image} />
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={Users}
+        renderItem={({ item }) =>
+          <TouchableOpacity onPress={() => { console.log(item.firstName) }} style={styles.userContainer} >
+            <Text style={styles.user} >{item.firstName} {item.lastName} </Text>
+            <Text style={styles.user} > Tamamladı: {randomNumber} / {randomNumber}</Text>
+          </TouchableOpacity>}
+      />
     </View >
   );
 };
@@ -20,16 +39,47 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  title: {
+    fontSize: wp(8),
+    fontWeight: 'bold',
+    color: '#0C1844',
+    alignSelf: 'center',
+  },
   image: {
     width: wp('10%'),
     height: wp('10%'),
-    tintColor: 'black',
+    tintColor: Colors.PRIMARY,
 
   },
   imageContainer: {
     padding: 10,
     alignSelf: 'flex-end',
     borderRadius: 30,
+  },
+  user: {
+    fontSize: 20,
+    color: Colors.WHITE
+  },
+  userContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    alignSelf: 'flex-start',
+    borderRadius: 8,
     marginHorizontal: wp(5),
-  }
+
+    width: wp('90%'),
+    height: hp(10),
+    marginBottom: 5,
+    backgroundColor: Colors.PRIMARY,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
 });
